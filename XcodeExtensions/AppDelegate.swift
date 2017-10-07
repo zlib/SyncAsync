ff5784ffce5b6f09353c3acd4bbeafa4c3ff8d39
Mikhail Motyzhenkov <m.motyzhenkov@gmail.com>
2017-10-07 17:28:25 +0300

    Initial Commit

---
+++ XcodeExtensions/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XcodeExtensions
+//
+//  Created by Михаил Мотыженков on 07.10.2017.
+//  Copyright © 2017 Михаил Мотыженков. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XcodeExtensions/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeExtensions
+//
+//  Created by Михаил Мотыженков on 07.10.2017.
+//  Copyright © 2017 Михаил Мотыженков. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
